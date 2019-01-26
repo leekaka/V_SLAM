aae93225e23b023cb98a0bd12908a1ea6d69d6ac
leekaka <lika27@163.com>
2019-01-26 23:07:05 +0800

    更新sophus

---
+++ slam_14/ch4/useSophus/build/CMakeFiles/useSophus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kali/kaka/MyGithub/V_SLAM/slam_14/ch4/useSophus/useSophus.cpp" "/home/kali/kaka/MyGithub/V_SLAM/slam_14/ch4/useSophus/build/CMakeFiles/useSophus.dir/useSophus.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/kali/kaka/slambook/3rdparty/Sophus"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ slam_14/ch4/useSophus/build/CMakeFiles/useSophus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useSophus.dir/useSophus.cpp.o"
+  "useSophus.pdb"
+  "useSophus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/useSophus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
