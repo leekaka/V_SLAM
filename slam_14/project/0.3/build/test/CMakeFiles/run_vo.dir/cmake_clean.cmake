11dbc9b1906169561dd66781444bb9732fa91ffd
leekaka <lika27@163.com>
2019-02-19 23:08:44 +0800

    补充

---
+++ slam_14/project/0.3/build/test/CMakeFiles/run_vo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_vo.dir/run_vo.cpp.o"
+  "../../bin/run_vo.pdb"
+  "../../bin/run_vo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
