aa1e44faa68c241e0f0b66658835049c78f4d325
leekaka <lika27@163.com>
2019-02-19 22:56:09 +0800

    增加project

---
+++ slam_14/project/0.1/build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kali/kaka/MyGithub/slam/project/0.1/src/camera.cpp" "/home/kali/kaka/MyGithub/slam/project/0.1/build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/kali/kaka/MyGithub/slam/project/0.1/src/config.cpp" "/home/kali/kaka/MyGithub/slam/project/0.1/build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/kali/kaka/MyGithub/slam/project/0.1/src/frame.cpp" "/home/kali/kaka/MyGithub/slam/project/0.1/build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/kali/kaka/MyGithub/slam/project/0.1/src/map.cpp" "/home/kali/kaka/MyGithub/slam/project/0.1/build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/kali/kaka/MyGithub/slam/project/0.1/src/mappoint.cpp" "/home/kali/kaka/MyGithub/slam/project/0.1/build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/home/kali/kaka/slambook/3rdparty/Sophus"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ slam_14/project/0.1/build/src/CMakeFiles/myslam.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myslam.dir/frame.cpp.o"
+  "CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "CMakeFiles/myslam.dir/map.cpp.o"
+  "CMakeFiles/myslam.dir/camera.cpp.o"
+  "CMakeFiles/myslam.dir/config.cpp.o"
+  "../../lib/libmyslam.pdb"
+  "../../lib/libmyslam.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
